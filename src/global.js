2ea44d0ca3d6cf57f6c9f39560f3514a2568ce3a
santosh-ksh7 <santoshksh7@gmail.com>
2022-07-13 06:45:05 +0530

    Using the heroku URL for the API link now

--- src/global.js
+++ src/global.js
@@ -1,1 +1,3 @@
-export const API = "https://62aaf0f8a62365888bd041ae.mockapi.io"
+// export const API = "https://62aaf0f8a62365888bd041ae.mockapi.io";
+export const API = "https://node-express-santosh.herokuapp.com";
+// export const API = "http://localhost:4000"
